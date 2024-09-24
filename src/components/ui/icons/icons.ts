a83e36ac2ccd2a29c0c82ede72b624be87024936
Tambouil <v.berceaux@gmail.com>
2024-09-24 21:42:24 +0200

    feat: contact page

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -526,4 +526,32 @@ export const Icons = {
     strokeLinejoin: 'round',
     stroke: 'currentColor',
   },
+  sun: {
+    paths: [
+      {
+        d: 'M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z',
+      },
+    ],
+    class: 'mt-1 h-6 w-6 flex-shrink-0 stroke-neutral-700 dark:stroke-neutral-500',
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    strokeWidth: '1.5',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    stroke: 'currentColor',
+  },
+  winter: {
+    paths: [
+      {
+        d: 'M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z',
+      },
+    ],
+    class: 'mt-1 h-6 w-6 flex-shrink-0 stroke-neutral-700 dark:stroke-neutral-500',
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    strokeWidth: '1.5',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    stroke: 'currentColor',
+  },
 };
